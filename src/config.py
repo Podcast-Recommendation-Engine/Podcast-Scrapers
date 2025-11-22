96825e7d971822ecf2fea2181b632005517a636e
Ibrahimghali <ibrahim.elghali@outlook.com>
2025-11-22 21:34:48 +0100

    refactor: Remove ffmpeg installation from Dockerfile and update docker-compose.yml for external volumes

--- src/config.py
+++ src/config.py
@@ -1,4 +1,6 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 RSS_FEEDS = [
     "https://lexfridman.com/feed/podcast/",
@@ -8,7 +10,7 @@ CHUNK_SIZE = 8192
 RAW_PATH = "data/raw"
 AUDIO_PATH = "data/audio"
 JSON_FILENAME = "podcasts.json"
-NUMBER_OF_PODCAST_FOR_EACH_PODCAST= 1
+NUMBER_OF_PODCAST_FOR_EACH_PODCAST= 2
 
 
 KAFKA_URL= os.getenv('KAFKA_URL', 'kafka')
