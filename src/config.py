0b984bfbfa8ba8484368faf4871b6aa261d2fe94
Ibrahimghali <ibrahim.elghali@outlook.com>
2025-11-30 09:23:40 +0100

    refactor: Comment out unused RSS feed URLs and update podcast count to 1

--- src/config.py
+++ src/config.py
@@ -4,22 +4,22 @@ load_dotenv()
 
 RSS_FEEDS = [
     "https://lexfridman.com/feed/podcast/",
-    "https://feeds.megaphone.fm/GLT1412515089",
-    "https://feeds.simplecast.com/qm_9xx0g",
-    "https://feeds.simplecast.com/54nAGcIl",
-    "https://feeds.simplecast.com/mKn_QmLS",
-    "https://feeds.megaphone.fm/thispastweekend",
-    "https://podcastfeeds.nbcnews.com/HL4TzgYC",
-    "https://www.thisamericanlife.org/podcast/rss.xml",
-    "https://feeds.simplecast.com/6Qp23t6h",
-    "https://feeds.simplecast.com/hNaFxXpO",
-    "https://rss.art19.com/new-heights",
+    # "https://feeds.megaphone.fm/GLT1412515089",
+    # "https://feeds.simplecast.com/qm_9xx0g",
+    # "https://feeds.simplecast.com/54nAGcIl",
+    # "https://feeds.simplecast.com/mKn_QmLS",
+    # "https://feeds.megaphone.fm/thispastweekend",
+    # "https://podcastfeeds.nbcnews.com/HL4TzgYC",
+    # "https://www.thisamericanlife.org/podcast/rss.xml",
+    # "https://feeds.simplecast.com/6Qp23t6h",
+    # "https://feeds.simplecast.com/hNaFxXpO",
+    # "https://rss.art19.com/new-heights",
 
 ]
 CHUNK_SIZE = 8192
 RAW_PATH = "data/bronze/metadata"
 AUDIO_PATH = "data/bronze/audio"
-NUMBER_OF_PODCAST_FOR_EACH_PODCAST= 10
+NUMBER_OF_PODCAST_FOR_EACH_PODCAST= 1
 
 
 KAFKA_URL= os.getenv('PS_KAFKA_URL', 'kafka')
